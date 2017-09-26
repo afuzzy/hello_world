eaace58b22b0d1fb3e270c5d24bce930996559fd
afuzzy <aognev@gmail.com>
2017-09-27 02:57:14 +1300

    Reiterated on comments. Changed window positioning from setGeometry to move and resize methods. Added a text editor for code and File Open/Exit menu

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from PyQt4 import QtGui
 
 
 class Window(QtGui.QWidget):
+#class Window(QtGui.QMainWindow):
     def __init__(self):
         # call for ancestor`s class __init__
         super(Window, self).__init__()
@@ -22,57 +23,168 @@ class Window(QtGui.QWidget):
         self.xcoord = 50
         self.ycoord = 60
         self.xsize = 250
-        self.ysize = 100
+        self.ysize = 300
         self.initWindowTitle = "Change me"
+
 
         # Create layout
         layout_box = QtGui.QFormLayout(self)
 
+        # menu bar
+        self.menuBar = QtGui.QMenuBar()
+        self.fileOpenAction = QtGui.QAction('&Open', self)
+        self.fileOpenAction.triggered.connect(self.openPyFile)
+        self.exitAction = QtGui.QAction('&Exit', self)
+        self.exitAction.triggered.connect(self.close_application)
+        self.fileMenu = self.menuBar.addMenu('&File')
+        self.fileMenu.addAction(self.fileOpenAction)
+        self.fileMenu.addAction(self.exitAction)
+        layout_box.addRow(self.menuBar)
+
+
         # validator for int inputs
         self.onlyInt = QtGui.QIntValidator()
+
 
         # input parameters list for window control
         windowParamsList = [["X coord", self.xcoord], ["Y coord", self.ycoord], ["X size", self.xsize],
                             ["Y size", self.ysize]]
 
-        self.label = []
-        self.edit = []
+        self.labels = []
+        self.edits = []
         # window position and size controls created inside loop based on list [label, initial_value]
+        # trying different loops here for better code readability
+        """ 
+        #old version       
         for param in windowParamsList:
-            self.label.append(QtGui.QLabel(windowParamsList[windowParamsList.index(param)][0], self))
-            self.edit.append(QtGui.QLineEdit(self))
-            self.edit[windowParamsList.index(param)].setValidator(self.onlyInt)
-            self.edit[windowParamsList.index(param)].setText(str(windowParamsList[windowParamsList.index(param)][1]))
-            self.edit[windowParamsList.index(param)].setMaximumWidth(30)
-            self.edit[windowParamsList.index(param)].editingFinished.connect(self.editingFinished)
-            layout_box.addRow(self.label[windowParamsList.index(param)], self.edit[windowParamsList.index(param)])
+            self.labels.append(QtGui.QLabel(windowParamsList[windowParamsList.index(param)][0], self))
+            self.edits.append(QtGui.QLineEdit(self))
+            self.edits[windowParamsList.index(param)].setValidator(self.onlyInt)
+            self.edits[windowParamsList.index(param)].setText(str(windowParamsList[windowParamsList.index(param)][1]))
+            self.edits[windowParamsList.index(param)].setMaximumWidth(30)
+            self.edits[windowParamsList.index(param)].editingFinished.connect(self.editingFinished)
+            layout_box.addRow(self.labels[windowParamsList.index(param)], self.edits[windowParamsList.index(param)])
+        """
 
-        # window tittle
-        self.label_windowName = QtGui.QLabel("Window title", self)
-        self.edit_WindowName = QtGui.QLineEdit(self)
-        self.edit_WindowName.setText(self.initWindowTitle)
-        self.edit_WindowName.editingFinished.connect(self.editingFinished)
-        layout_box.addRow(self.label_windowName, self.edit_WindowName)
 
-        # Set initial window geo
-        self.setGeometry(self.xcoord, self.ycoord, self.xcoord + self.xsize, self.ycoord + self.ysize)
+        """
+        # enumerate solution looks more readable of cource. It works!!!
+        for counter,param in enumerate(windowParamsList):
+            self.labels.append(QtGui.QLabel(param[0],self))
+            self.edits.append(QtGui.QLineEdit(self))
+            self.edits[counter].setValidator(self.onlyInt)
+            self.edits[counter].setText(str(param[1]))
+            self.edits[counter].setMaximumWidth(30)
+            self.edits[counter].editingFinished.connect(self.editingFinished)
+            layout_box.addRow(self.labels[counter], self.edits[counter])
+        """
+
+        """
+        # solution with iterating over both list fields! It works!!!
+        counter=0
+        for label,value in windowParamsList:
+            self.labels.append(QtGui.QLabel(label,self))
+            self.edits.append(QtGui.QLineEdit(self))
+            self.edits[counter].setValidator(self.onlyInt)
+            self.edits[counter].setText(str(value))
+            self.edits[counter].setMaximumWidth(30)
+            self.edits[counter].editingFinished.connect(self.editingFinished)
+            layout_box.addRow(self.labels[counter], self.edits[counter])
+            counter+=1
+        del counter
+        """
+
+        """        
+        # solution with enumerate and iterating over both list fields! It works!!!
+        # this looks amost elegant and readable
+        for counter, (label, value) in enumerate(windowParamsList):
+            self.labels.append(QtGui.QLabel(label, self))
+            self.edits.append(QtGui.QLineEdit(self))
+            self.edits[counter].setValidator(self.onlyInt)
+            self.edits[counter].setText(str(value))
+            self.edits[counter].setMaximumWidth(30)
+            self.edits[counter].editingFinished.connect(self.editingFinished)
+            layout_box.addRow(self.labels[counter], self.edits[counter])
+        """
+
+        # and last smart improvement, even better
+        for counter, (label, value) in enumerate(windowParamsList):
+            self.labels.append(QtGui.QLabel(label, self))
+            edit=QtGui.QLineEdit(self)
+            edit.setValidator(self.onlyInt)
+            edit.setText(str(value))
+            edit.setMaximumWidth(30)
+            edit.editingFinished.connect(self.editingFinished)
+            self.edits.append(edit)
+            layout_box.addRow(self.labels[counter], self.edits[counter])
+
+
+        # window tittle edit interface
+        self.label_windowTitle = QtGui.QLabel("Window title", self)
+        self.edit_WindowTitle = QtGui.QLineEdit(self)
+        self.edit_WindowTitle.setText(self.initWindowTitle)
+        self.edit_WindowTitle.editingFinished.connect(self.editingFinished)
+        layout_box.addRow(self.label_windowTitle, self.edit_WindowTitle)
+
+        #Code window
+        self.label_Code = QtGui.QLabel("Code to run", self)
+        self.edit_Code = QtGui.QTextEdit(self)
+        layout_box.addRow(self.label_Code, self.edit_Code)
+
+        #Run button
+        self.runButton = QtGui.QPushButton('Run', self)
+        self.runButton.clicked.connect(self.runButtonAction)
+        layout_box.addRow(self.runButton)
+
+
+        # Set initial window geo and title
+        self.move(int(self.edits[0].text()), int(self.edits[1].text()))
+        self.resize(int(self.edits[2].text()), int(self.edits[3].text()))
         self.setWindowTitle(self.initWindowTitle)
 
+        """    
+        #old way (did not respect frame)
+        def editingFinished(self):
+            self.setGeometry(int(self.edits[0].text()), int(self.edits[1].text()), int(self.edits[2].text()),
+                             int(self.edits[3].text()))
+            self.setWindowTitle(self.edit_WindowTitle.text())
 
+        def moveEvent(self, QMoveEventMy):
+            self.edits[0].setText(str(self.geometry().x()))
+            self.edits[1].setText(str(self.geometry().y()))
+            super(Window, self).moveEvent(QMoveEventMy)
+        """
+
+
+    #new way, now it respects window framing
     def editingFinished(self):
-        self.setGeometry(int(self.edit[0].text()), int(self.edit[1].text()), int(self.edit[2].text()),
-                         int(self.edit[3].text()))
-        self.setWindowTitle(self.edit_WindowName.text())
 
-    def moveEvent(self, QMoveEventMy):
-        self.edit[0].setText(str(self.geometry().x()))
-        self.edit[1].setText(str(self.geometry().y()))
-        super(Window, self).moveEvent(QMoveEventMy)
+        self.move(int(self.edits[0].text()), int(self.edits[1].text()))
+        self.resize(int(self.edits[2].text()), int(self.edits[3].text()))
+        self.setWindowTitle(self.edit_WindowTitle.text())
 
-    def resizeEvent(self, QResizeEventMy):
-        self.edit[2].setText(str(self.geometry().width()))
-        self.edit[3].setText(str(self.geometry().height()))
-        super(Window, self).resizeEvent(QResizeEventMy)
+    #new way, not sure if it is better
+    def moveEvent(self, QMoveEvent):
+        self.edits[0].setText(str(self.pos().x()))
+        self.edits[1].setText(str(self.pos().y()))
+        super(Window, self).moveEvent(QMoveEvent)
+
+
+    def resizeEvent(self, QResizeEvent):
+        self.edits[2].setText(str(self.size().width()))
+        self.edits[3].setText(str(self.size().height()))
+        super(Window, self).resizeEvent(QResizeEvent)
+
+    def close_application(self):
+        exit()
+
+    def openPyFile(self):
+        fileName = QtGui.QFileDialog.getOpenFileName(self, 'OpenFile')
+        text = open(fileName).read()
+        self.edit_Code.setText(text)
+    def runButtonAction(self):
+        code=str(self.edit_Code.toPlainText())
+        exec(code)
 
 
 # main check added to make sure we are running a program, not just importing module
@@ -80,8 +192,4 @@ if __name__ == "__main__":
     app = QtGui.QApplication([])
     GUI = Window()
     GUI.show()
-    # testing with second window
-    GUI2 = Window()
-    GUI2.show()
-
     exit(app.exec_())
